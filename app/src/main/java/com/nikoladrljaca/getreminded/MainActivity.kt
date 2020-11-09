f272f954e517bb8f2476c2b8b159f122ce23b273
nikolaDrljaca <devnikoladr@gmail.com>
2020-11-09 21:42:05 +0300

    Theme changes

--- app/src/main/java/com/nikoladrljaca/getreminded/MainActivity.kt
+++ app/src/main/java/com/nikoladrljaca/getreminded/MainActivity.kt
@@ -7,6 +7,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.PreferenceManager
@@ -37,8 +38,10 @@ class MainActivity : AppCompatActivity() {
         val darkModeToggle = shared.getBoolean("dark_mode_switch", false)
         if (darkModeToggle) {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            window.decorView.systemUiVisibility = 0
         } else {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
         }
     }
 
